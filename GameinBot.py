cbb169a4872b1184f634dee3d75ee87d07d401cb
Erfan <erfanamani2000@gmail.com>
2022-08-29 09:56:18 +0430

    Initialization

---
+++ GameinBot.py
@@ -0,0 +1,22 @@
+import telebot
+import pandas as pd
+
+class Player:
+    def __init__(self):
+        pass
+
+    def purchase_raw_material(self):
+        pass
+
+    def produce(self):
+        pass
+
+class Gamein(Player):
+    def __init__(self):
+        super().__init__()
+
+    def sale_raw_material(self):
+        pass
+
+    def purchase_form_player(self):
+        pass
